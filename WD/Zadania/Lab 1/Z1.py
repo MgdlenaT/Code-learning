038749c39da9c9db305bef2c035a8d20160ff8bb
JakubSM <46352779+JakubSM99@users.noreply.github.com>
2022-05-10 17:14:33 +0200

    ad

--- WD/Zadania/Lab 1/Z1.py
+++ WD/Zadania/Lab 1/Z1.py
@@ -1,1 +1,31 @@
-Zadania
+# Zadanie nr1.
+#Napisz pierwszy skrypt, w ktorym zadeklarujesz zmienne każdego typu, a następnie wyświetl te zmienne.
+
+# Zmienne typu int
+
+a = 1
+b = 2
+
+# Zmienne typu Float
+c = 3.14
+d = 5.17
+
+# Zmienne typu String
+e = 'ab'
+f = 'cd'
+
+# Zmienne typu Boolean
+g = False
+h = True
+
+# Zmienne typu None
+i = None
+
+# Zmienne typu Kolekcja
+# 1: Lista
+j = [1,2,3,4,5,6,7,8,9,0]
+# 2: 
+
+
+
+print(a, b, c, d, e, f)
